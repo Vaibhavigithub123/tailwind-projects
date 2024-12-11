a30055cfb6fcd7562d9f2e163aed2bfce6ebf63a
Vaibhavi Suradkar <vaibhavisuradkar105@gmail.com>
2024-12-11 12:47:17 +0530

    Branding-site

---
+++ Branding-site/dist/script.js
@@ -0,0 +1,14 @@
+gsap.registerPlugin(ScrollTrigger)
+
+
+gsap.from("#img",{opacity:0, y:-50, duration:1.5})
+
+var vl = gsap.timeline({
+    scrollTrigger:{
+        trigger:"#part2",
+        start: "top 50%",
+        end:"50% 50%",
+        markers:true
+    }
+})
+vl.from("#img1",{opacity:0, y:-50, duration:1.5} )
